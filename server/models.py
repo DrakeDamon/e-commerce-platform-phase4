f3959b68b2179f30b1d43f6f5d0d671a93482989
Drake Damon <dddamon06@gmail.com>
2025-03-18 18:26:27 -0400

    created notes for relationship

--- server/models.py
+++ server/models.py
@@ -20,6 +20,8 @@ class User(db.Model, SerializerMixin):
   created_at = db.Column(db.DateTime, server_default=db.func.now())
 
   #Relationships
+  #back_populates creates a relationship between Product and user
+  #cascade happens when a user is deleted
   products = db.relationship('Product', back_populates='seller', cascade='all, delete-orphan')
   orders = db.relationship('Order', back_populates='buyer', cascade='all, delete-orphan')
 
