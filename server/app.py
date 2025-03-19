490915f30a66163aa30a2a4501a5fc612311b187
Drake Damon <dddamon06@gmail.com>
2025-03-18 20:09:08 -0400

    added root route

--- server/app.py
+++ server/app.py
@@ -3,3 +3,8 @@ from flask_restful import Resource
 
 from config import app, db, api
 from models import User, Product, Category, ProductCategory, Order
+
+# Root route for testing
+@app.route('/')
+def index():
+    return '<h1>Stylish Clothing Store API</h1>'
