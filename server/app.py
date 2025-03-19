83396a3287b510e0e5905b691493bcd50e271519
Drake Damon <dddamon06@gmail.com>
2025-03-18 20:11:08 -0400

    created authentication routes

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-Copyfrom flask import request, session, jsonify
+from flask import request, session, jsonify
 from flask_restful import Resource
 
 from config import app, db, api
@@ -35,3 +35,77 @@ class Users(Resource):
         
         except Exception as e:
             return {'error': str(e)}, 400
+
+
+class UserById(Resource):
+    def get(self, id):
+        user = User.query.filter_by(id=id).first()
+        
+        if not user:
+            return {'error': 'User not found'}, 404
+            
+        return user.to_dict(), 200
+    
+    def patch(self, id):
+        user = User.query.filter_by(id=id).first()
+        
+        if not user:
+            return {'error': 'User not found'}, 404
+        
+        data = request.get_json()
+        
+        for attr in data:
+            if attr == 'password':
+                user.password_hash = data[attr]
+            elif hasattr(user, attr):
+                setattr(user, attr, data[attr])
+        
+        db.session.commit()
+        
+        return user.to_dict(), 200
+    
+    def delete(self, id):
+        user = User.query.filter_by(id=id).first()
+        
+        if not user:
+            return {'error': 'User not found'}, 404
+            
+        db.session.delete(user)
+        db.session.commit()
+        
+        return '', 204  
+
+# Authentication Routes
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        
+        user = User.query.filter_by(username=data.get('username')).first()
+        
+        if not user or not user.check_password(data.get('password')):
+            return {'error': 'Invalid username or password'}, 401
+            
+        session['user_id'] = user.id
+        
+        return user.to_dict(), 200
+
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        
+        if not user_id:
+            return {'error': 'Not authorized'}, 401
+            
+        user = User.query.filter_by(id=user_id).first()
+        
+        if not user:
+            return {'error': 'User not found'}, 404
+            
+        return user.to_dict(), 200     
+
+      
